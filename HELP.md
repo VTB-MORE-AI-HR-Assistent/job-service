b71373dca3e4f4fdea7169c47ffc9566029efe37
Рафаэль <106482482+Aris-turtle@users.noreply.github.com>
2025-08-30 01:46:56 +0300

    init commit

---
+++ HELP.md
@@ -0,0 +1,32 @@
+# Read Me First
+The following was discovered as part of building this project:
+
+* The JVM level was changed from '24' to '21' as the Kotlin version does not support Java 24 yet.
+* The original package name 'com.aristurtle.job-service' is invalid and this project uses 'com.aristurtle.job_service' instead.
+
+# Getting Started
+
+### Reference Documentation
+For further reference, please consider the following sections:
+
+* [Official Gradle documentation](https://docs.gradle.org)
+* [Spring Boot Gradle Plugin Reference Guide](https://docs.spring.io/spring-boot/3.5.5/gradle-plugin)
+* [Create an OCI image](https://docs.spring.io/spring-boot/3.5.5/gradle-plugin/packaging-oci-image.html)
+* [Spring Web](https://docs.spring.io/spring-boot/3.5.5/reference/web/servlet.html)
+* [Spring Data JPA](https://docs.spring.io/spring-boot/3.5.5/reference/data/sql.html#data.sql.jpa-and-spring-data)
+* [Spring Data JDBC](https://docs.spring.io/spring-boot/3.5.5/reference/data/sql.html#data.sql.jdbc)
+
+### Guides
+The following guides illustrate how to use some features concretely:
+
+* [Building a RESTful Web Service](https://spring.io/guides/gs/rest-service/)
+* [Serving Web Content with Spring MVC](https://spring.io/guides/gs/serving-web-content/)
+* [Building REST services with Spring](https://spring.io/guides/tutorials/rest/)
+* [Accessing Data with JPA](https://spring.io/guides/gs/accessing-data-jpa/)
+* [Using Spring Data JDBC](https://github.com/spring-projects/spring-data-examples/tree/master/jdbc/basics)
+
+### Additional Links
+These additional references should also help you:
+
+* [Gradle Build Scans – insights for your project's build](https://scans.gradle.com#gradle)
+
---
+++ build.gradle.kts
@@ -0,0 +1,67 @@
+plugins {
+	kotlin("jvm") version "1.9.25"
+	kotlin("plugin.spring") version "1.9.25"
+	id("org.springframework.boot") version "3.5.5"
+	id("io.spring.dependency-management") version "1.1.7"
+	kotlin("plugin.jpa") version "1.9.25"
+}
+
+group = "com.aristurtle"
+version = "0.0.1-SNAPSHOT"
+description = "AI HR job and vacancy service"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
+	implementation("org.springdoc:springdoc-openapi-starter-common:2.2.0")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("jakarta.validation:jakarta.validation-api:3.1.1")
+	implementation("org.modelmapper:modelmapper:3.2.3")
+	runtimeOnly("org.postgresql:postgresql")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	testImplementation("org.junit.jupiter:junit-jupiter-api")
+	testImplementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	testImplementation("org.hibernate.validator:hibernate-validator")
+	testImplementation("org.glassfish:jakarta.el:4.0.2") // для валидации в тестах
+	testImplementation("org.springframework.boot:spring-boot-starter-test") {
+		exclude(group = "org.mockito", module = "mockito-core")
+	}
+	testImplementation("org.mockito.kotlin:mockito-kotlin:5.2.1")
+	testImplementation("com.ninja-squad:springmockk:4.0.2") // Альтернатива Mockito для Kotlin
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
+
+allOpen {
+	annotation("jakarta.persistence.Entity")
+	annotation("jakarta.persistence.MappedSuperclass")
+	annotation("jakarta.persistence.Embeddable")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+
+tasks.test {
+	useJUnitPlatform()
+}
